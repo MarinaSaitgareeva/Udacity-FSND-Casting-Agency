b4f9c844f4089788e8698c17239f6e9d04a654e4
Marina Saitgareeva <4006761ms@gmail.com>
2022-12-08 22:43:10 -0500

    add post and patch routes

--- backend/app.py
+++ backend/app.py
@@ -15,13 +15,7 @@ collections.MutableMapping = collections.abc.MutableMapping
 collections.MutableSet = collections.abc.MutableSet
 collections.Callable = collections.abc.Callable
 
-from flask import (
-    Flask,
-    request,
-    jsonify,
-    abort
-)
-import json
+from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 
 from database.models import (
@@ -30,7 +24,7 @@ from database.models import (
     setup_migrations,
     Actor,
     Movie,
-    Casting
+    Casting,
 )
 from auth.auth import AuthError, requires_auth
 
@@ -46,51 +40,40 @@ def create_app(test_config=None):
     """
     CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-
-    '''
+    """
     @TODO uncomment the following line to initialize the database
     !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
     !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
     !! Running this function will add data
-    '''
+    """
     with app.app_context():
         db_drop_and_create_all()
-
 
     """
     Use the after_request decorator to set Access-Control-Allow
     """
+
     @app.after_request
     def after_request(response):
-        '''
+        """
         CORS Headers
-        '''
+        """
         response.headers.add(
-            "Access-Control-Allow-Headers",
-            "Content-Type,Authorization,true"
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
         )
         response.headers.add(
-            "Access-Control-Allow-Methods",
-            "GET,PUT,POST,DELETE,OPTIONS"
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
         )
 
         return response
-
 
     """
     Routes
     """
 
-
     @app.route("/")
     def index():
-        return jsonify(
-            {
-                "succes": True,
-                "message": "Welcome to our Casting Agency"
-            }
-        )
-
+        return jsonify({"succes": True, "message": "Welcome to our Casting Agency"})
 
     @app.route("/actors", methods=["GET"])
     def retrieve_actors():
@@ -98,12 +81,20 @@ def create_app(test_config=None):
 
         if len(actors) == 0:
             abort(404)
-                
-        return jsonify({
-            "success": True,
-            "actors": [actor.format_json() for actor in actors]
-        })
-    
+
+        return jsonify(
+            {"success": True, "actors": [actor.format_json() for actor in actors]}
+        )
+
+    @app.route("/actors/<int:actor_id>", methods=["GET"])
+    def retrieve_actor(actor_id):
+        actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+
+        if actor is None:
+            abort(404)
+
+        return jsonify({"success": True, "actor": actor.format_json()})
+
     @app.route("/movies", methods=["GET"])
     # @requires_auth("get:movies")
     def retrieve_movies():
@@ -111,29 +102,42 @@ def create_app(test_config=None):
 
         if len(movies) == 0:
             abort(404)
-        
-        return jsonify({
-            "success": True,
-            "movies": [movie.format_json() for movie in movies]
-        })
+
+        return jsonify(
+            {"success": True, "movies": [movie.format_json() for movie in movies]}
+        )
+
+    @app.route("/movies/<int:movie_id>", methods=["GET"])
+    # @requires_auth("get:movies")
+    def retrieve_movie(movie_id):
+        movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+
+        if movie is None:
+            abort(404)
+
+        return jsonify({"success": True, "movie": movie.format_json()})
 
     @app.route("/actors/create", methods=["POST"])
-    @requires_auth("post:actors")
-    def add_actor(payload):
+    # @requires_auth("post:actors")
+    def add_actor():
         body = request.get_json()
 
-        if not ("first_name" in body or
-                "last_name" in body or
-                "age" in body or
-                "gender" in body or
-                "email" in body or
-                "phone" in body or
-                "photo_link" in body or
-                "seeking_movie" in body):
+        if not (
+            "first_name" in body
+            or "last_name" in body
+            or "fullname" in body
+            or "age" in body
+            or "gender" in body
+            or "email" in body
+            or "phone" in body
+            or "photo_link" in body
+            or "seeking_movie" in body
+        ):
             abort(422)
 
         first_name = body.get("first_name", None)
         last_name = body.get("last_name", None)
+        fullname = body.get("fullname", None)
         age = body.get("age", None)
         gender = body.get("gender", None)
         email = body.get("email", None)
@@ -145,34 +149,39 @@ def create_app(test_config=None):
             actor = Actor(
                 first_name=first_name,
                 last_name=last_name,
+                fullname=fullname,
                 age=age,
                 gender=gender,
                 email=email,
                 phone=phone,
                 photo_link=photo_link,
-                seeking_movie=seeking_movie
+                seeking_movie=seeking_movie,
             )
             actor.insert()
-            return jsonify({
-                "success": True,
-                "added_actor_id": actor.id,
-                "added_actor_full_name": actor.fullname,
-                "actors": len(Actor.query.order_by(Actor.fullname).all())
-            })
+
+            return jsonify(
+                {
+                    "success": True,
+                    "added_actor_id": actor.id,
+                    "added_actor_full_name": actor.fullname,
+                    "actors_total": len(Actor.query.all()),
+                }
+            )
 
         except Exception:
             abort(422)
 
-
     @app.route("/movies/create", methods=["POST"])
-    @requires_auth("post:movies")
-    def add_movie(payload):
+    # @requires_auth("post:movies")
+    def add_movie():
         body = request.get_json()
 
-        if not ("title" in body or
-                "genres" in body or
-                "release_date" in body or
-                "seeking_actor" in body):
+        if not (
+            "title" in body
+            or "genres" in body
+            or "release_date" in body
+            or "seeking_actor" in body
+        ):
             abort(422)
 
         title = body.get("title", None)
@@ -185,15 +194,105 @@ def create_app(test_config=None):
                 title=title,
                 genres=genres,
                 release_date=release_date,
-                seeking_actor=seeking_actor
+                seeking_actor=seeking_actor,
             )
             movie.insert()
-            return jsonify({
-                "success": True,
-                "added_movie_id": movie.id,
-                "added_movie_title": movie.title,
-                "movies": len(Movie.query.order_by(Movie.release_date, Movie.title).all())
-            })
+
+            return jsonify(
+                {
+                    "success": True,
+                    "added_movie_id": movie.id,
+                    "added_movie_title": movie.title,
+                    "movies_total": len(Movie.query.all()),
+                }
+            )
+
+        except Exception:
+            abort(422)
+
+    @app.route("/actors/<int:actor_id>", methods=["PATCH"])
+    # @requires_auth("patch:actors")
+    def modify_actor(actor_id):
+        body = request.get_json()
+
+        if not (
+            "first_name" in body
+            or "last_name" in body
+            or "fullname" in body
+            or "age" in body
+            or "gender" in body
+            or "email" in body
+            or "phone" in body
+            or "photo_link" in body
+            or "seeking_movie" in body
+        ):
+            abort(422)
+
+        first_name = body.get("first_name", None)
+        last_name = body.get("last_name", None)
+        fullname = body.get("fullname", None)
+        age = body.get("age", None)
+        gender = body.get("gender", None)
+        email = body.get("email", None)
+        phone = body.get("phone", None)
+        photo_link = body.get("photo_link", None)
+        seeking_movie = body.get("seeking_movie", None)
+
+        try:
+            actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+
+            if actor is None:
+                abort(404)
+
+            actor.first_name = first_name
+            actor.last_name = last_name
+            actor.fullname = fullname
+            actor.age = age
+            actor.gender = gender
+            actor.email = email
+            actor.phone = phone
+            actor.photo_link = photo_link
+            actor.seeking_movie = seeking_movie
+
+            actor.update()
+
+            return jsonify({"success": True, "modified_actor": actor.format_json()})
+
+        except Exception:
+            abort(422)
+
+    @app.route("/movies/<int:movie_id>", methods=["PATCH"])
+    # @requires_auth("patch:movies")
+    def modify_movie(movie_id):
+        body = request.get_json()
+
+        if not (
+            "title" in body
+            or "genres" in body
+            or "release_date" in body
+            or "seeking_actor" in body
+        ):
+            abort(422)
+
+        title = body.get("title", None)
+        genres = body.get("genres", None)
+        release_date = body.get("release_date", None)
+        seeking_actor = body.get("seeking_actor", None)
+
+        try:
+            movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+
+            if movie is None:
+                abort(404)
+
+            movie.title = title
+            movie.genres = genres
+            movie.release_date = release_date
+            movie.seeking_actor = seeking_actor
+
+            movie.update()
+
+            return jsonify({"success": True, "modified_movie": movie.format_json()})
 
         except Exception:
             abort(422)
@@ -202,66 +301,59 @@ def create_app(test_config=None):
     Error handler for 400, 404, 405, 422, 500
     """
 
-
     @app.errorhandler(400)
     def bad_request(error):
-        return jsonify({
-            "success": False,
-            "error": 400,
-            "message": "Bad Request"
-        }), 400
-
+        return jsonify({"success": False, "error": 400, "message": "Bad Request"}), 400
 
     @app.errorhandler(404)
     def not_found(error):
-        return jsonify({
-            "success": False,
-            "error": 404,
-            "message": "Resource Not Found"
-        }), 404
-
+        return (
+            jsonify({"success": False, "error": 404, "message": "Resource Not Found"}),
+            404,
+        )
 
     @app.errorhandler(405)
     def method_not_allowed(error):
-        return jsonify({
-            "success": False,
-            "error": 405,
-            "message": "Method Not Allowed"
-        }), 405
-    
+        return (
+            jsonify({"success": False, "error": 405, "message": "Method Not Allowed"}),
+            405,
+        )
 
     @app.errorhandler(422)
     def unprocessable(error):
-        return jsonify({
-            "success": False,
-            "error": 422,
-            "message": "Unprocessable resource"
-        }), 422
-
+        return (
+            jsonify(
+                {"success": False, "error": 422, "message": "Unprocessable resource"}
+            ),
+            422,
+        )
 
     @app.errorhandler(500)
     def internal_server(error):
-        return jsonify({
-            "success": False,
-            "error": 500,
-            "message": "Internal server error"
-        }), 500
-
+        return (
+            jsonify(
+                {"success": False, "error": 500, "message": "Internal server error"}
+            ),
+            500,
+        )
 
     """
     Error handler for AuthError
     """
 
-
     @app.errorhandler(AuthError)
     def handle_auth_error(error):
-        return jsonify({
-            "success": False,
-            "error": error.status_code,
-            "message": error.error["description"]
-        }), error.status_code
+        return (
+            jsonify(
+                {
+                    "success": False,
+                    "error": error.status_code,
+                    "message": error.error["description"],
+                }
+            ),
+            error.status_code,
+        )
 
-    
     return app
 
 
@@ -269,4 +361,4 @@ def create_app(test_config=None):
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
