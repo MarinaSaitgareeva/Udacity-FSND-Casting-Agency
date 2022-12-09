5b124fb2a55177c9cbc8b29da4ebfa7cc8eb95e1
Marina Saitgareeva <4006761ms@gmail.com>
2022-12-09 13:16:11 -0500

    update global variables

--- backend/setup.sh
+++ backend/setup.sh
@@ -5,10 +5,13 @@ export DATABASE_TEST_URI="postgresql://marinas:2001:5432/casting_agency_test"
 export FLASK_APP=app.py
 export FLASK_ENV=development
 
-export AUTH0_DOMAIN=""
-export API_AUDIENCE=""
-export ALGORITHMS=['RS256']
-export SECRET=""
+export AUTH0_DOMAIN="fs2022nd.us.auth0.com"
+export API_AUDIENCE="Casting_Agency_FSND"
+export ALGORITHMS=["RS256"]
+
+export CLIENT_ID="HtJZ7kKWkhIqL3l306Lq0G2JHlaTnZqY"
+export CLIENT_SECRET="wYCRfDg4e6NbsjjTEARnGWJVk4A4kmoXtg045O9D6bZrPzmbeyZO5sptJEPJBgns"
+export CALLBACK_URI="http://127.0.0.1:8080"
 
 
 echo "setup.sh script executed successfully!"
