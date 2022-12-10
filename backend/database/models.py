d0bac25611b9d8df69d3c31f48f8de972af34ff2
Marina Saitgareeva <4006761ms@gmail.com>
2022-12-09 23:21:05 -0500

    style changes

--- backend/database/models.py
+++ backend/database/models.py
@@ -23,9 +23,9 @@ from datetime import datetime
 load_dotenv()
 
 DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_PATH = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 db = SQLAlchemy()
